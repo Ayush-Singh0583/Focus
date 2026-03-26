ecff750ee5cbd64b2e7c8a81403be0ca0b9368a3
Ayush Singh <competetiveprogramming.study@gmail.com>
2026-03-26 12:33:28 +0530

    deploy v5

--- focusly/frontend/vite.config.js
+++ focusly/frontend/vite.config.js
@@ -6,14 +6,14 @@ export default defineConfig({
 
   base: '/Focus/',   // MUST be outside server
 
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  }
+  // server: {
+  //   port: 5173,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:5000',
+  //       changeOrigin: true,
+  //       secure: false
+  //     }
+  //   }
+  // }
 })
